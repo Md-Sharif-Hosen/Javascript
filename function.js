ce16f50c70fa8633becf2a70a51f3bda1a90cfb1
sharif-hosen <mdsharifkhan762@gmail.com>
2024-03-24 21:07:56 +0600

    Constructor function

--- function.js
+++ function.js
@@ -79,3 +79,17 @@ myName("Sharif ","Ahmed");
     document.write("WhatsApp");
 })();
 
+*/
+5.Constructor FUnction
+function Person(first, last, age, eye) {
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eyeColor = eye;
+}
+
+// Create a Person object
+const myFather = new Person("John", "Doe", 50, "blue");
+// Display age
+document.getElementById("demo").innerHTML =
+"My father is " + myFather.age + "." +"His Favvourite color is "+ myFather.eyeColor; 
