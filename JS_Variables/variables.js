14d5796ee2fbbfeff123a2a962ded0c24a38423f
sharif-hosen <mdsharifkhan762@gmail.com>
2024-03-25 20:54:19 +0600

    var variables

---
+++ JS_Variables/variables.js
@@ -0,0 +1,57 @@
+//Javascript Variables
+
+/*
+ *1. JavaScript variable names are case-sensitive
+ !2. Variable names cannot contain spaces.
+ *3. Variables cannot be the same as reserved keywords such as `if` or `const`.
+ *4. By convention, JavaScript variable names are written in camelCase.
+ *5. Variables should be given descriptive names that indicate their content and usage (e.g. `sellingPrice` and `costPrice` rather than `x` and `y`).
+ *6. You can not add number first
+ *7. _, $ is acceptable but - not accaptable
+ */
+
+ /*
+ *Var Varables:
+ *1. You can re declare
+ *2. You can re assaign
+ *3. Global Scooped
+ *4. var is hosted
+ */
+
+ 
+//  You can re declare:
+ /*
+var a =40;
+var a=50;
+var a ="Bangladesh";
+console.log(a);
+
+You can re assign:
+
+ var a=500;
+ var b="sharif Bhai";
+ b="Sharif AHmed";
+ console.log(a);
+ console.log(b);
+
+Global Scooped:
+
+var a=50;
+var b="Sharif";
+{  
+    c=150;
+    var f=400;
+    console.log(b);
+    console.log(a);
+}
+console.log(c);
+console.log(f);
+
+var is hosted:
+
+console.log(hello);
+var hello="sharif";
+var hello;
+console.log(hello)
+
+*/
