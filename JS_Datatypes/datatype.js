400bfecddf84759dbf2f7edec5286e0b57db2a12
sharif-hosen <mdsharifkhan762@gmail.com>
2024-03-27 12:23:43 +0600

    javascript operator precedence

--- JS_Datatypes/datatype.js
+++ JS_Datatypes/datatype.js
@@ -105,3 +105,11 @@ let car ={
 car.model="toyota";
 console.log(car)
 console.log( typeof car)
+
+let a=10; b=10; c=10;
+if(a==b==c)
+{
+    console.log("hello")
+}else{
+console.log("Bye bye")
+}
---
+++ JS_Operator_precedence/operator_precedencs.js
@@ -0,0 +1,14 @@
+// JavaScript Operator precedence
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_precedence
+// ----- () {} []
+// / * - +
+console.log(5+59*60-40/2);
+
+
+/*
+5 + 6 - 3 / 2 * 4
+= 5+6-1.5*4
+= 5+6-6
+= 5 
+console.log(5 + 6 - 3 / 2 * 4); // Ans: 5
+*/
