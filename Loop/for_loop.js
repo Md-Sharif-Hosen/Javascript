a59686c50df919e331e5e88c462e654c8e0f2197
sharif-hosen <mdsharifkhan762@gmail.com>
2024-05-10 17:10:12 +0600

    javascript loop

---
+++ Loop/for_loop.js
@@ -0,0 +1,20 @@
+// let i;
+// for(i=0; i<10; i=i+2){
+//     console.log( i+" Submit");
+// }
+
+//! loop break continue
+// for(let i=0 ; i<10; i=i+1){
+//     if(i==5 || i==8){
+//         continue;
+//     }
+//     console.log( i+" Submit");
+// }
+
+
+for(let i=0 ; i<10; i=i+1){
+    if(i===5){
+        break;
+    }
+    console.log( i+" Submit");
+}
